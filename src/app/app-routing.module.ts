56545a4810482ba58d8c6c051915222bd3162a65
chengkangzai <pycck@hotmail.com>
2021-01-15 10:51:06 +0800

    clean file
    Signed-off-by: chengkangzai <pycck@hotmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,21 +4,8 @@ import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 const routes: Routes = [
     {
         path: '',
-        redirectTo: 'recipes',
         pathMatch: 'full'
     },
-    {
-        path: 'recipes',
-        children: [
-            {
-                path: '',
-                loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesPageModule),
-            }, {
-                path: ':recipeId',
-                loadChildren: () => import('./recipes/recipe-detail/recipe-detail.module').then(m => m.RecipeDetailPageModule)
-            }
-        ]
-    }
 ];
 
 @NgModule({
